51e469e379cdcdf3e0475ff927757b2f741fb2b1
suyako <30832854+suyako-to-be-a-programmer@users.noreply.github.com>
2018-05-12 19:02:15 +0800

    2018.5.12 SMO方法基本完成

--- svm/_base.py
+++ svm/_base.py
@@ -1,14 +1,30 @@
 import numpy as np
 from utils.validation import _num_samples
 
-def select(i, X):
+
+def select(i, X, error):
+    """启发式选择第二个代更新alpha的index
+
+    选择方式依据如下：
+    由于alpha2_new = alpha2 + y2 * (error1 - error2) / enta，所以选择abs(error1 - error2)更大
+    的更新alpha的幅度也大->损失函数下降越快
+    :param i:
+    :param X:
+    :param error:
+    :return:
+    """
     if not isinstance(i, int):
         raise TypeError()
-    n_samples = _num_samples(X)
-    index = i
-    while index == i:
-        index = np.random.randint(0, n_samples)
-    return index
+    # n_samples = _num_samples(X)
+    if error[i] > 0:
+        j = np.where(error == min(error))
+    else:
+        j = np.where(error == max(error))
+    # j = i
+    # while j == i:
+    #     j = np.random.randint(0, n_samples)
+    return j
+
 
 def linear(x1, x2):
     return np.dot(x1, x2)
--- svm/classes.py
+++ svm/classes.py
@@ -3,10 +3,13 @@ from utils.validation import _num_samples
 
 from ._base import select, linear
 
+
 class SVC(object):
 
-    def __init__(self, C):
+    def __init__(self, C, kernel='linear', tol=1e-3):
         self.C = C
+        self.kernel = kernel
+        self.tol = tol
 
     @staticmethod
     def _clip_alpha(alpha_new, low, high):
@@ -26,43 +29,98 @@ class SVC(object):
         :return:
         """
         n_samples = _num_samples(X)
-        alphas = np.zeros(n_samples)
-        b = 0
+        self.alphas = np.zeros(n_samples)
+        self.b = 0
         C = self.C
-        w = np.dot(X.T, alphas * y)
+        self.w = np.dot(X.T, self.alphas * y)
+        u = np.dot(X, self.w) - self.b
+        self.error = u - y
         for iter_num in range(100):
+            if iter_num:
+                index = np.where(0 < self.alphas < C)  # 提取所有可能的支持向量，之后的alpha更新都在这些向量中进行
+            else:
+                index = np.arange(n_samples)
+            alphas = self.alphas[index]  # python浅拷贝机制，在alphas中任意元素的改变都会导致self.alphas对应元素的改变
+            X_curr = X[index]
+            y_curr = y[index]
+            u_curr = np.dot(X_curr, self.w) - self.b
+            error_curr = u_curr - y_curr
+            self.error[index] = error_curr
+            r_curr = error_curr * y_curr
             for i, alpha1 in enumerate(alphas):
-                cost = np.dot(w.T, w) / 2 - np.sum(alphas)
-                u1 = np.dot(X[i, :], w) - b
-                E1 = u1 - y[i]
-                j = select(i, X)  # TODO(suyako): 之后采用启发式，当前为随机选择
-                alpha2 = alphas[j]
-                u2 = np.dot(X[j, :], w) - b
-                E2 = u2 - y[j]
-                if y[i] == y[j]:
-                    L = max(0, alpha2 + alpha1 - C)
-                    H = min(C, alpha2 + alpha1)
-                else:
-                    L = max(0, alpha2 - alpha1)
-                    H = min(C, C + alpha2 - alpha1)
-                K11 = linear(X[i, :], X[i, :])
-                K22 = linear(X[j, :], X[j, :])
-                K12 = linear(X[i, :], X[j, :])
-                enta = K11 + K22 - 2 * K12  # 二阶导数
-                alphas[j] = self._clip_alpha(alpha2 + y[j] * (E1 - E2) / enta, L, H)
-                alphas[i] += y[i] * y[j] * (alpha2 - alphas[j])
-                b1 = E1 + y[i] * (alphas[i] - alpha1) * K11 + y[j] * (alphas[j] - alpha2) * K12 + b
-                b2 = E2 + y[i] * (alphas[i] - alpha1) * K12 + y[j] * (alphas[j] - alpha2) * K22 + b
-                if 0 < alphas[i] and C > alphas[i]:
-                    # this condition means X[i] is the support vector, thus w.T * X[i] + b = y[i]
-                    b = b1
-                elif 0 < alphas[j] and C > alphas[j]:
-                    # this condition means X[j] is the support vector, thus w.T * X[j] + b = y[j]
-                    b = b2
-                else:
-                    # this means X[i] and X[j] neither to be the support vector, then the hyperplane between
-                    # with the hyperplanes corresponding to X[i] & X[j] are consistent with the KKT conditions.
-                    b = (b1 + b2) / 2
-                w += y[i] * (alphas[i] - alpha1) * X[i, :] + y[j] * (alphas[j] - alpha2) * X[j, :]
-        w = np.dot(X.T, alphas * y)
-        return w
+                # i的启发式选择,显然，在这里，当alpha1不等于C或者0时，意味着X[i]较可能为支持向量，则我们改变
+                # alpha[i]所带来的受益会更大（alpha[i]与w息息相关，对非支持向量更新alpha后可能依然为C或者0，
+                # 这时w不会改变）。同时，因为支持向量满足uy=1，所以如果已经满足uy=1则不进行更新（给了一个tol的
+                # 允许误差范围）
+                # if (r_curr[i] < - self.tol and alpha1 < C) or (r_curr[i] > self.tol and alpha1 > 0):
+                if abs(r_curr[i]) > self.tol:
+                    fai = np.dot(self.w.T, self.w) / 2 - np.sum(alphas)
+                    j = select(i, X_curr, error_curr)
+                    alpha2 = alphas[j]
+                    K = (linear(X_curr[i], X_curr[i]), linear(X_curr[i], X_curr[j]), linear(X_curr[j], X_curr[j]))
+                    alphas[i], alphas[j] = self._update_alpha(i, j, y_curr, K, error_curr, alphas)
+                    delta_alpha = alphas[i] - alpha1, alphas[j] - alpha2
+                    self.b = self._update_b(i, j, y_curr, K, delta_alpha, error_curr, alphas)
+                    self.w = self._update_w(i, j, X_curr, y_curr, delta_alpha)
+                    # u = np.dot(X, self.w) - self.b
+                    # self.error = u - y
+
+    def _update_alpha(self, i, j, y, K, error, alphas):
+        """update the alpha about i and j
+
+        :return:
+        """
+        C = self.C
+        y1, y2 = y[i], y[j]
+        error1, error2 = error[i], error[j]
+        alpha1, alpha2 = alphas[i], alphas[j]
+        if y1 == y2:
+            low = max(0, alpha2 + alpha1 - C)
+            high = min(C, alpha2 + alpha1)
+        else:
+            low = max(0, alpha2 - alpha1)
+            high = min(C, C + alpha2 - alpha1)
+        if low == high:
+            return alpha1, alpha2
+        s = y1 * y2
+        k11, k12, k22 = K[0], K[1], K[2]
+        enta = k11 + k22 - 2 * k12  # enta为cost关于alpha2的二阶导数
+        if enta > 0:  # enta大于0意味着cost存在最小解
+            alpha2_new = self._clip_alpha(alpha2 + y2 * (error1 - error2) / enta, low, high)
+        else:
+            f1 = y1 * (error1 + self.b) - alpha1 * k11 - s * alpha2 * k12
+            f2 = y2 * (error2 + self.b) - s * alpha1 * k12 - alpha2 * k22
+            l1 = alpha1 + s * (alpha2 - low)
+            h1 = alpha1 + s * (alpha2 - high)
+            fai__l = l1 * f1 + low * f2 + l1 ** 2 * k11 / 2 + low ** 2 * k22 / 2 + s * low * l1 * k12
+            fai__h = h1 * f1 + high * f2 + h1 ** 2 * k11 / 2 + high ** 2 * k22 / 2 + s * high * h1 * k12
+            if fai__l < fai__h:
+                alpha2_new = low
+            elif fai__l > fai__h:
+                alpha2_new = high
+            else:
+                alpha2_new = alpha2
+        if alpha2 == alpha2_new:
+            return alpha1, alpha2
+        alpha1_new = alpha1 + s * (alpha2 - alpha2_new)
+        return alpha1_new, alpha2_new
+
+    def _update_b(self, i, j, y, K, delta_alpha, error, alphas):
+        C = self.C
+        k11, k12, k22 = K[0], K[1], K[2]
+        delta_alpha1, delta_alpha2 = delta_alpha[0], delta_alpha[1]
+        b1 = error[i] + y[i] * delta_alpha1 * k11 + y[j] * delta_alpha2 * k12 + self.b
+        b2 = error[j] + y[i] * delta_alpha1 * k12 + y[j] * delta_alpha2 * k22 + self.b
+        if 0 < alphas[i] < C:
+            # this condition means X[i] is the support vector, thus w.T * X[i] + b = y[i]
+            return b1
+        elif 0 < alphas[j] < C:
+            # this condition means X[j] is the support vector, thus w.T * X[j] + b = y[j]
+            return b2
+        else:
+            # this means X[i] and X[j] neither to be the support vector, then the hyperplane between
+            # with the hyperplanes corresponding to X[i] & X[j] are consistent with the KKT conditions.
+            return (b1 + b2) / 2
+
+    def _update_w(self, i, j, X, y, delta_alpha):
+        return self.w + y[i] * delta_alpha[0] * X[i] + y[j] * delta_alpha[1] * X[j]
