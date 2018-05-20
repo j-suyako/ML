2da60a6001c05c497c47f1ca459efb252de6f285
suyako <30832854+suyako-to-be-a-programmer@users.noreply.github.com>
2018-05-20 19:08:01 +0800

    2018.5.20 improve tree

--- tree/tree.py
+++ tree/tree.py
@@ -1,9 +1,7 @@
 from abc import ABCMeta, abstractmethod
 import copy
-import matplotlib.pyplot as plt
 import numpy as np
 from queue import Queue
-from sklearn.datasets import load_iris
 from utils.validation import check_array, Check_X_y, _num_samples
 
 
@@ -25,27 +23,21 @@ def _median(X):
 class Node(object):
     __metaclass__ = ABCMeta
 
-    def __init__(self, X, y, depth=0, attributes=None, max_depth=None):
-        X, y = Check_X_y(X, y)
+    def __init__(self, X, y, depth=0, max_depth=None):
+        # X, y = Check_X_y(X, y)
         self.X = X  # 每个节点对应的训练集
         self.y = y  # 训练集对应的样本分类
         self.depth = depth  # 每个节点在树中的高度，初始化为0
-        n_samples, n_features = X.shape
+        self.max_depth = max_depth
+        n_samples = X.shape[0]
+        self.n_samples = n_samples  # 训练集数量
         classifiers, count = np.unique(y, return_counts=True)
         self.freq_max_y = classifiers[0] if len(classifiers) > 0 else classifiers  # 该节点中样本最多的类，主要在predict中使用
-        self.n_samples = n_samples  # 训练集数量
-        self.children = {}  # 训练集的子节点，由split动态生成
-        if not attributes:
-            attributes = list(range(n_features))
-        self.attributes = attributes  # attributes代表属性集，如果缺省的话给一个0 - N-1的列表，其中N是X的属性集数量
-        self.max_depth = max_depth
-        features = list(range(n_features))
-        self.features = features
-        self.split_features = None
-        # self.split_features = self.attributes[0]  # 训练集的最优划分属性
         self.classifier = classifiers[0] if len(classifiers) == 1 else None  # 该节点所对应的类别，如果y中有不止一个分类则返回None
-        # self.feature_chosen = np.zeros(n_features, dtype=bool)
-        self.feature_chosen = [False] * n_features  # 用来保存已经用来分割过的属性集
+        self.children = {}  # 训练集的子节点，由split动态生成
+        self.split_features = None
+        self.features = list()
+        self.feature_chosen = list()
 
     def ent(self):
         """该样本集合的信息熵
@@ -61,11 +53,41 @@ class Node(object):
         return entropy_of_sample
 
     @abstractmethod
-    def depth_first_fit(self):
+    def _feature_choose(self, *args):
         pass
 
+    def gain(self, *args):
+        res = self.ent()
+        for _, each in self._feature_choose(*args):
+            res -= each.n_samples / self.n_samples * each.ent()
+        return res
+
     @abstractmethod
+    def get_best_split_feature(self):
+        pass
+
+    def depth_first_fit(self):
+        if self.classifier is None:
+            best_features = self.get_best_split_feature()
+            self.set_split_features(*best_features)
+            for child in self.children:
+                self.children[child].fit()
+
     def breadth_first_fit(self):
+        BFS = Queue()
+        BFS.put(self)
+        depth = 0
+        while depth < self.max_depth and not BFS.empty():
+            currNode = BFS.get()
+            if currNode.classifier is None:
+                best_features = currNode.get_best_split_feature()  # 选择最优属性
+                currNode.set_split_features(*best_features)  # 用该最优属性进行划分
+                for child in currNode.children:
+                    BFS.put(currNode.children[child])
+            depth = currNode.depth
+
+    @abstractmethod
+    def set_split_features(self, *args):
         pass
 
     def fit(self):
@@ -82,8 +104,12 @@ class NodeByID3(Node):
     只能用于非连续属性的划分（实际上二分法对这个应该没有限制，我也不懂为什么都说ID3不能处理连续值）
     """
 
-    def __init__(self, X, y, depth=0, attributes=None, max_depth=None):
-        super(NodeByID3, self).__init__(X, y, depth=depth, attributes=attributes, max_depth=max_depth)
+    def __init__(self, X, y, depth=0, max_depth=None):
+        super(NodeByID3, self).__init__(X, y, depth=depth, max_depth=max_depth)
+        n_samples, n_features = X.shape
+        for i in range(n_features):
+            self.features.append(np.unique(X[:, i]))
+            self.feature_chosen.append(False)
 
     def _feature_choose(self, features_k):
         """选择X的第k个属性进行分割，生成Dv
@@ -95,68 +121,29 @@ class NodeByID3(Node):
         """
         if features_k < 0 or features_k >= len(self.features):
             raise ValueError("No such features.")
-        for attr in np.unique(self.X[:, features_k]):
+        for attr in self.features[features_k]:
+            attr = "X[{}] = {}".format(features_k, attr)
             index = (self.X[:, features_k] == attr)
             X = self.X[index]  # 找到每个属性值包含的训练集
             y = self.y[index]  # 该属性值包含的训练集对应的类别
-            yield attr, NodeByID3(X, y, self.depth + 1, self.features)
+            yield attr, NodeByID3(X, y, self.depth + 1, self.max_depth)
 
-    def gain(self, features_k):
-        """计算用第k个属性对样本进行划分得到的信息增益
-
-        :param features_k:
-        :return:
-        """
-        res = self.ent()
-        for _, each in self._feature_choose(features_k):
-            res -= each.n_samples / self.n_samples * each.ent()
-        return res
-
-    def depth_first_fit(self):
-        """fit操作
-
-        :return:
-        """
-        # 递归返回条件，如果所有样本属于同一个类别则不做处理，相当于标记为叶节点
-        if self.classifier is None:
-            Gain = list()
-            Gain_index = list()
-            for k, _ in enumerate(self.features):
-                # 如果该属性已经在之前被选择过作为划分依据，则跳过
-                if self.feature_chosen[k]:
-                    continue
-                Gain.append(self.gain(k))  # 计算每个属性划分所带来的信息增益并存入Gain
-                Gain_index.append(k)  # 同时记录被用来试验的属性的index
-            best_features = Gain_index[Gain.index(max(Gain))]  # 选择最优属性
-            self.set_split_features(best_features)  # 用该最优属性进行划分
-            # 在已选择属性向量中增加该最优属性（Attention！每个分支节点对应的已选择属性都是不一样的）
-            for child in self.children:
-                self.children[child].fit()  # 递归
-
-    def breadth_first_fit(self):
-        BFS = Queue()
-        BFS.put(self)
-        depth = 0
-        while depth < self.max_depth and not BFS.empty():
-            currNode = BFS.get()
-            if currNode.classifier is None:
-                Gain = list()
-                Gain_index = list()
-                for k, _ in enumerate(self.features):
-                    # 如果该属性已经在之前被选择过作为划分依据，则跳过
-                    if currNode.feature_chosen[k]:
-                        continue
-                    Gain.append(self.gain(k))  # 计算每个属性划分所带来的信息增益并存入Gain
-                    Gain_index.append(k)  # 同时记录被用来试验的属性的index
-                best_features = Gain_index[Gain.index(max(Gain))]  # 选择最优属性
-                currNode.set_split_features(best_features)  # 用该最优属性进行划分
-                for child in currNode.children:
-                    BFS.put(currNode.children[child])
-            depth = currNode.depth + 1
-
+    def get_best_split_feature(self):
+        if self.classifier is not None:
+            raise ValueError
+        Gain = list()
+        Gain_index = list()
+        for k, _ in enumerate(self.features):
+            # 如果该属性已经在之前被选择过作为划分依据，则跳过
+            if self.feature_chosen[k]:
+                continue
+            Gain.append(self.gain(k))  # 计算每个属性划分所带来的信息增益并存入Gain
+            Gain_index.append(k)  # 同时记录被用来试验的属性的index
+        return Gain_index[Gain.index(max(Gain))]  # 选择最优属性
 
     def set_split_features(self, features_k):
-        self.split_features = self.attributes[features_k]
+        # self.split_features = self.attributes[features_k]
+        self.split_features = features_k
         for attr, each in self._feature_choose(features_k):
             self.children[attr] = each  # child中保存由该最优属性划分得到的每个属性值对应的叶节点
             # self.children[attr].feature_chosen[self.feature_chosen] = True
@@ -180,15 +167,13 @@ class NodeByC4_dot_5(Node):
     最优属性划分采用信息熵增益率的方法（注意与ID3信息熵增益的区别）
     可以处理连续变量
     """
-    def __init__(self, X, y, depth=0, attributes=None, max_depth=None):
-        super(NodeByC4_dot_5, self).__init__(X, y, depth=depth, attributes=attributes, max_depth=max_depth)
-        for i, each in enumerate(self.features):
+    def __init__(self, X, y, depth=0, max_depth=None):
+        super(NodeByC4_dot_5, self).__init__(X, y, depth=depth, max_depth=max_depth)
+        n_samples, n_features = X.shape
+        for i in range(n_features):
             values = np.unique(X[:, i])
-            each = list()
-            for median_value in _median(values):
-                each.append(median_value)
-            self.features[i] = each
-            self.feature_chosen[i] = [False] * len(each)
+            self.features.append(list(_median(values)))
+            self.feature_chosen.append([False] * len(values))
 
     def _feature_choose(self, features_k, value_v):
         """选择X的第k个属性进行分割，生成Dk
@@ -207,28 +192,12 @@ class NodeByC4_dot_5(Node):
             if not i:
                 X = self.X[index]
                 y = self.y[index]
-                attr = "{} <= {}".format(self.attributes[features_k], self.features[features_k][value_v])
+                attr = "X[{}] <= {}".format(features_k, self.features[features_k][value_v])
             else:
                 X = self.X[~index]
                 y = self.y[~index]
-                attr = "{} > {}".format(self.attributes[features_k], self.features[features_k][value_v])
-            yield attr, NodeByC4_dot_5(X, y, self.depth + 1, self.attributes)
-        # for each_value in np.unique(self.X[:, features_k]):
-        #     index = (self.X[:, features_k] == each_value)
-        #     X = self.X[index]  # 找到每个属性值包含的训练集
-        #     y = self.y[index]  # 该属性值包含的训练集对应的类别
-        #     yield each_value, NodeByC4_dot_5(X, y, self.attributes)
-
-    def gain(self, features_k, value_v):
-        """计算用第k个属性对样本进行划分得到的信息增益
-
-        :param features_k:
-        :return:
-        """
-        res = self.ent()
-        for _, each in self._feature_choose(features_k, value_v):
-            res -= each.n_samples / self.n_samples * each.ent()
-        return res
+                attr = "X[{}] > {}".format(features_k, self.features[features_k][value_v])
+            yield attr, NodeByC4_dot_5(X, y, self.depth + 1, self.max_depth)
 
     def _IV(self, features_k):
         """属性k的固有值（intrinsic value）
@@ -243,48 +212,26 @@ class NodeByC4_dot_5(Node):
         value_samples_freq = value_samples / n_samples
         return -np.sum(value_samples_freq * np.log2(value_samples_freq))
 
-    def gain_ratio(self, features_k, value_v):
-        gain = self.gain(features_k, value_v)
+    def gain_ratio(self, args):
+        features_k, value_v = args
+        if self.feature_chosen[features_k][value_v]:
+            return 0
+        gain = self.gain(*args)
         IV = self._IV(features_k)
         if not gain or not IV:
             return 0
-        return self.gain(features_k, value_v) / self._IV(features_k)
+        return gain / IV
 
-    def depth_first_fit(self):
-        if self.classifier is None:
-            Gain_ratio = list()
-            Gain_ratio_index = list()
-            for i, feature in enumerate(self.features):
-                for j, value in enumerate(feature):
-                    if self.feature_chosen[i][j]:
-                        continue
-                    Gain_ratio.append(self.gain_ratio(i, j))
-                    Gain_ratio_index.append((i, j))
-            features_k, value_v = Gain_ratio_index[Gain_ratio.index(max(Gain_ratio))]
-            self.set_split_features(features_k, value_v)
-            for each_child in self.children:
-                self.children[each_child].fit()
-
-    def breadth_first_fit(self):
-        BFS = Queue()
-        BFS.put(self)
-        depth = 0
-        while depth < self.max_depth and not BFS.empty():
-            currNode = BFS.get()
-            if currNode.classifier is None:
-                Gain_ratio = list()
-                Gain_ratio_index = list()
-                for i, feature in enumerate(currNode.features):
-                    for j, value in enumerate(feature):
-                        if currNode.feature_chosen[i][j]:
-                            continue
-                        Gain_ratio.append(currNode.gain_ratio(i, j))
-                        Gain_ratio_index.append((i, j))
-                features_k, value_v = Gain_ratio_index[Gain_ratio.index(max(Gain_ratio))]
-                currNode.set_split_features(features_k, value_v)
-                for child in currNode.children:
-                    BFS.put(currNode.children[child])
-            depth = currNode.depth
+    def get_best_split_feature(self):
+        if self.classifier is not None:
+            raise ValueError()
+        Gain_ratio_index = list()
+        for i, feature in enumerate(self.features):
+            p = [i] * len(feature)
+            q = range(len(feature))
+            Gain_ratio_index.extend(list(zip(p, q)))
+        index, _ = max(enumerate(map(self.gain_ratio, Gain_ratio_index)), key=lambda p: p[1])
+        return Gain_ratio_index[index]
 
     def set_split_features(self, features_k, value_v):
         self.split_features = (features_k, value_v)
@@ -292,7 +239,7 @@ class NodeByC4_dot_5(Node):
         for i, (attr, each) in enumerate(self._feature_choose(features_k, value_v)):
             self.children[attr] = each
             self.children[attr].feature_chosen = copy.deepcopy(self.feature_chosen)
-            while value_v < len(self.feature_chosen[features_k]) and value_v >= 0:
+            while 0 <= value_v < len(self.feature_chosen[features_k]):
                 self.children[attr].feature_chosen[features_k][value_v] = True
                 if not i:
                     value_v += 1
@@ -309,9 +256,9 @@ class DecisionTreeClassifier(object):
 
     def fit(self, X, y):
         if self.criterion == 'ID3':
-            self.tree_ = NodeByID3(X, y, attributes=self.attributes, max_depth=self.max_depth)
+            self.tree_ = NodeByID3(X, y, max_depth=self.max_depth)
         elif self.criterion == 'C4.5':
-            self.tree_ = NodeByC4_dot_5(X, y, attributes=self.attributes, max_depth=self.max_depth)
+            self.tree_ = NodeByC4_dot_5(X, y, max_depth=self.max_depth)
         self.tree_.fit()
 
     def predict(self, X):
@@ -329,18 +276,8 @@ class DecisionTreeClassifier(object):
             currNode = self.tree_
             while currNode.classifier is None and currNode.depth < max_depth:  # 节点不是叶节点
                 features_k, value_v = currNode.split_features
-                if e[features_k] <= currNode.features[features_k][value_v]:
-                    symbol = "<="
-                else:
-                    symbol = ">"
-                attr = "{} {} {}".format(currNode.attributes[features_k],
-                                         symbol, currNode.features[features_k][value_v])
-                # attr = X[currNode.attributes.index(currNode.split_features)]
+                symbol = "<=" if e[features_k] <= currNode.features[features_k][value_v] else ">"
+                attr = "X[{}] {} {}".format(features_k, symbol, currNode.features[features_k][value_v])
                 currNode = currNode.children[attr]
-                # if currNode.children[attr]:
-                #     currNode = currNode.children[attr]
-                # else:
-                #     # 若分支节点中没有对应的属性值，则该样本标记为该分支节点中样本最多的类
-                #     return currNode.freq_max_y
             res[i] = currNode.classifier if currNode.classifier else currNode.freq_max_y
         return res
